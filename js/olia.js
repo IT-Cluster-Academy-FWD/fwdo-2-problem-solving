e4499a4cc98f6fcffb2f47adb29aaff003596ae3
deletko <deletko>
2020-09-04 21:36:19 +0300

    after lesson

--- js/olia.js
+++ js/olia.js
@@ -1,8 +1,15 @@
-const $input = document.getElementById('my-input');
+/*const $input = document.getElementById('my-input');
 const $output = document.getElementById('my-output');
 
 $input.addEventListener('input', function() {
   //console.log($input.value);
   $output.innerHTML=$input.value;
 
+})*/
+const $firstname = document.getElemementById('#fname');
+const $lastname = document.getElemementById('#lname');
+const $emailadress = document.getElemementById('#email');
+
+$form.addEventListener ('submit', function (event) {
+  event.preventDefault();
 })
--- js/pavlo.js
+++ js/pavlo.js
@@ -1,6 +1,6 @@
-const $input = document.getElementById('my-input');
-const $output = document.getElementById('my-input');
+// const $input = document.getElementById('my-input');
+// const $output = document.getElementById('my-input');
 
-$input.addEventListener('input', function () {
-  console.log($input.value);
-});
+// $input.addEventListener('input', function () {
+//   console.log($input.value);
+// });
