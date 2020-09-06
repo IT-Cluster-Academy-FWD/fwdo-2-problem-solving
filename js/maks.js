d83acd356d62874eef9550953d6db0ac7e7e0770
maks <makskoasr79@gmail.com>
2020-09-06 23:33:40 +0300

    js with iput

--- js/maks.js
+++ js/maks.js
@@ -5,6 +5,41 @@
 // $input.addEventListener('input', function() {
 //   console.log();
 //   $output.innerHTML = $input.value;
-// });
+// })
+const $form = document.getElementById('form');
+const $firstNameInput = document.getElementById('fname-input');
+const $lastName = document.getElementById('lname-input');
+const $email = document.getElementById('email-input');
+const $userInfo = document.getElementById('user-container');
+const $button = document.getElementById('button');
 
-const $firstNameInput = 
+
+
+// $button.addEventListener('click', function(event) {
+//    event.preventDefault();
+//    userValue();
+//    disableinput();
+// });
+$button.addEventListener('submit', function(event) {
+   event.preventDefault();
+   userValue();
+   disableinput();
+});
+
+
+function disableinput() {
+   const $firstNameInput = document.getElementById('fname-input');
+   const $lastName = document.getElementById('lname-input');
+   const $email = document.getElementById('email-input');
+
+   $firstNameInput.disabled = true;
+   $lastName.disabled = true;
+   $email.disabled = true;
+}
+
+function userValue(){
+   $userInfo.innerHTML = $userInfo.innerHTML + '<p>' + $firstNameInput.value + '</p>' ;
+   $userInfo.innerHTML = $userInfo.innerHTML + '<p>' + $lastName.value + '<p>';
+   $userInfo.innerHTML = $userInfo.innerHTML + '<p>' + $email.value + '</p>';   
+}
+   
